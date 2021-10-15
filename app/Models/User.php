0f74de2733441ad54384f0ea58c6fe27557945f4
Bálint Lovas <lovasb93@gmail.com>
2021-10-15 15:55:38 +0200

    User modification

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,11 @@ class User extends Authenticatable
      * @var string[]
      */
     protected $fillable = [
-        'name',
+        'firstName',
+        'lastName',
         'email',
         'password',
+        'role'
     ];
 
     /**
