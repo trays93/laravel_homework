cae3e41f5970b52274e1f920433e0771d21e4d9d
Bálint Lovas <lovasb93@gmail.com>
2021-10-17 11:42:22 +0200

    Users database seeder added

---
+++ database/seeders/AddUsers.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AddUsers extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'firstName' => 'John',
+            'lastName' => 'Doe',
+            'email' => 'john.doe@gmail.com',
+            'password' => Hash::make('secret'),
+            'role' => 'admin'
+        ]);
+
+        DB::table('users')->insert([
+            'firstName' => 'Jane',
+            'lastName' => 'Doe',
+            'email' => 'jane.doe@gmail.com',
+            'password' => Hash::make('secret'),
+            'role' => 'user'
+        ]);
+    }
+}
