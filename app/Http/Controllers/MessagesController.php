aaeeb3c84e2e0a1a0f100c0641cad5cbbef13b04
Bálint Lovas <lovasb93@gmail.com>
2021-12-03 11:05:58 +0100

    Message list view

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MessagesController extends Controller
 {
     public function index()
     {
-        return view('messages.index', []);
+        $messages = Message::with('sender')
+            ->with('to')
+            ->where('to_id', '=', Auth::user()->id)
+            ->orderBy('created_at', 'DESC')
+            ->get();
+
+        return view('messages.index', [
+            'messages' => $messages
+        ]);
     }
 
     public function writeMessage()
--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -1,1 +1,36 @@
-messages
+@extends('layout.app')
+
+@section('title', 'Mailbox')
+
+@section('content')
+    <div class="row">
+        <div class="col-12 col-lg-12">
+            <div class="card mt-5 mb-5">
+                <div class="card-header">
+                    <h1>@yield('title')</h1>
+                </div>
+                <div class="card-body">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Body</th>
+                                <th>From</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($messages as $message)
+                                <tr>
+                                    <td>{{ $message['title'] }}</td>
+                                    <td>{{ $message['body'] }}</td>
+                                    <td>{{ $message['sender']['firstName'] }} {{ $message['sender']['lastName'] }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            
+        </div>
+    </div>    
+@endsection
